0072c689e2bd858bd2a1bb64c45f9b16620dd5b6
juliema <julieallen34@gmail.com>
2016-05-10 16:58:44 -0500

    fixing code issues and adding comments
    making the tree line more general

--- BrueeliaMS/removeburnin.pl
+++ BrueeliaMS/removeburnin.pl
@@ -2,6 +2,13 @@
 
 use strict;
 use warnings;
+#################################################
+#       will create an output file of all the trees post a predefined burnin generation
+#       will print out either nexus or newick format. 
+#        usage perl removeburnin.pl 2000000 <TREEFILE>  <OUTPUTFILE>
+#
+#################################################
+
 
 my $burnin=shift;  ## generation number not total number of samples to remove
 my $treefile=shift;
@@ -15,7 +22,7 @@ my $postnumber=0;
 open FH, "<$treefile";
 open OUT, ">$outfile";
 while (<FH>) { 
-    if (/^tree\s+STATE_(\d+)/) {
+    if (/tree\s+gen.(\d+)/) {
 	my $gen=$1;
 	$countree++;
 	if ($gen > $burnin) {
